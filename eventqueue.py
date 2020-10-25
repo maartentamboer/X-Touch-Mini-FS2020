27608a08bee2ea05eae3e1086e1f8b7a385ee4f6
Vincent Bouret <vbouret>
2020-10-25 18:18:57 -0400

    Add EventQueue to support multiple events and manual events with specific value.

---
+++ eventqueue.py
@@ -0,0 +1,33 @@
+from SimConnect import *
+
+class SingleEvent(object):
+    def __init__(self, _ae: AircraftEvents, _eventname, _type="auto", _value=0, _description=""):
+        self.eventname = _eventname
+        self.type = _type
+        self.value = _value
+        self.description = _description
+        if (_type == "manual"):
+            self.event = Event(_eventname.encode(), _ae.sm) #manual event
+        else:
+            self.event = _ae.find(_eventname) #autofind event
+            
+
+    def __call__(self, value=0):
+        if self.type == "manual":
+            self.event(self.value)
+        else:
+            self.event(value)
+           
+
+class EventQueue(object):
+
+    def __init__(self):
+        self.event_list = []
+
+    def __call__(self, value=0):
+        for event in self.event_list:
+            if event:
+                event(value)
+
+    def add(self, _event: SingleEvent):
+        self.event_list.append(_event)
