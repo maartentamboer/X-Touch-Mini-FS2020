ddbb940f892f74c05a150f8ee56bceddf0ca746f
Maarten Tamboer <1395437+maartentamboer@users.noreply.github.com>
2020-10-31 13:44:01 +0100

    remove unused var

--- main.py
+++ main.py
@@ -74,7 +74,6 @@ def main_app(offline: bool):
         control_change_dict[f.control_channel] = f
 
     triggers[0].on_simvar_data(1.0)
-    count = 0
     while True:
         for obj in GlobalStorage().all_elements:
             if obj.bound_simvar and aq:
